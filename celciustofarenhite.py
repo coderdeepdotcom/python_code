3006122c39857f485334ea60bed76b00ec5bb755
coderdeepdotcom <144016561+coderdeepdotcom@users.noreply.github.com>
2023-10-03 10:11:40 +0530

    Update celciustofarenhite.py

--- celciustofarenhite.py
+++ celciustofarenhite.py
@@ -1,8 +1,8 @@
 # Python Program to convert temperature in celsius to fahrenheit
 
 # change this value for a different result
-celsius = 37.5
+CELCIUS = 37.5
 
 # calculate fahrenheit
-fahrenheit = (celsius * 1.8) + 32
-print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+FAHRENHITE = (CELCIUS * 1.8) + 32
+print(f'{CELCIUS} degree Celsius is equal to {FAHRENHITE} degree Fahrenheit')
